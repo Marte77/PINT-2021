987e3e862adb128c027fd0ad6dde3a30d7bae317
Marte77 <67065789+Marte77@users.noreply.github.com>
2021-04-30 21:56:31 +0100

    pagina de registo

--- Android/CrowdZero_V000/app/src/main/java/com/example/crowdzero_v000/MainActivity.java
+++ Android/CrowdZero_V000/app/src/main/java/com/example/crowdzero_v000/MainActivity.java
@@ -6,21 +6,35 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button botaoLogin = findViewById(R.id.botaoEntrar);
-        botaoLogin.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                Intent i = new Intent(MainActivity.this,LoginActivity.class);
-                startActivity(i);
-            }
-        });
-        //colocar o texto com hiperligacao e os botoes a abrir as atividades
+        Button botaoEntrar = findViewById(R.id.botaoEntrar);
+        Button botaoRegisto = findViewById(R.id.botaoRegistoMain);
+        TextView hyperlink_sem_registo= findViewById(R.id.hiperlink_Sem_registo);
+
     }
 
+    public void onClick(View v){
+        int id = v.getId();
+        switch (id){
+            case R.id.botaoRegistoMain:
+                Intent RegistoIntent = new Intent(MainActivity.this,RegistoActivity.class);
+                startActivity(RegistoIntent);
+            break;
+            case R.id.botaoEntrar:
+                Intent EntrarIntent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(EntrarIntent);
+                break;
+            case R.id.hiperlink_Sem_registo:
+                Toast.makeText(getApplicationContext(),"a tua mae",Toast.LENGTH_LONG);
+                break;
+        }
+    }
 }
---
+++ Android/CrowdZero_V000/app/src/main/java/com/example/crowdzero_v000/RegistoActivity.java
@@ -0,0 +1,93 @@
+package com.example.crowdzero_v000;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class RegistoActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button cidadeBotao; //talvez alterar estes botoes para textviews iguais as outras para ficar tudo igual
+    Button dataNascimentoBotao;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registo);
+        cidadeBotao = findViewById(R.id.botaoCidadeRegisto);
+        dataNascimentoBotao = findViewById(R.id.botaoDataNascimentoRegisto);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.botaoCidadeRegisto:
+                    abrirCidadeDialog();
+                break;
+            case R.id.botaoDataNascimentoRegisto:
+                    abrirCalendario();
+                break;
+        }
+    }
+
+    public void abrirCalendario(){
+       MaterialDatePicker.Builder calendario = MaterialDatePicker.Builder.datePicker();
+       calendario.setTitleText(R.string.data_nascimento_registo);
+
+       final MaterialDatePicker datePicker = calendario.build();
+       datePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener() {
+           @Override
+           public void onPositiveButtonClick(Object selection) {
+               dataNascimentoBotao.setText(datePicker.getHeaderText());
+
+           }
+       });
+
+       datePicker.show(getSupportFragmentManager(), String.valueOf(R.string.data_nascimento_registo));
+    }
+
+    public void abrirCidadeDialog(){
+        final String[] cidades = {"Aveiro","Beja","Braga","Bragança","Castelo Branco","Coimbra","Évora","Faro","Guarda","Leiria","Lisboa","Portalegre","Porto","Santarém","Setúbal","Viana do Castelo","Vila Real","Viseu"};
+
+        final int opcao = 0;
+        MaterialAlertDialogBuilder dialogo = new MaterialAlertDialogBuilder(RegistoActivity.this);
+        dialogo.setTitle(R.string.cidade);
+        dialogo.setNeutralButton(R.string.cancelar, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+        dialogo.setPositiveButton(R.string.selecionar, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        dialogo.setSingleChoiceItems(cidades, opcao, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                cidadeBotao.setText(cidades[i]);
+            }
+        });
+        dialogo.show();
+
+    }
+
+}
