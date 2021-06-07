d3f5a1b0dcbefd113f1023aa7f1874c97cd459fa
hugoeomarques <75944913+hugoeomarques@users.noreply.github.com>
2021-06-07 17:31:32 +0100

    bd API

---
+++ Base_de_Dados/API/src/model/database.js
@@ -0,0 +1,17 @@
+var Sequelize = require('sequelize');
+const sequelize = new Sequelize(
+'dc46lifec8n9j9',
+'pjellcdlhqirbb',
+'d80db698597148f935fa7622540e2bb54db21434bd6106712fd3ec767ea0cc03', 
+{
+host: 'ec2-54-154-101-45.eu-west-1.compute.amazonaws.com',
+port: '5432',
+dialect: 'postgres'
+dialectOptions:{
+        ssl:{          
+            rejectUnauthorized: false        
+        }
+    }
+}
+);
+module.exports = sequelize;
