4dc2b976a6360da5073375d7b1f96ee5e11894ec
marto <martinho.tavares@live.com.pt>
2021-06-23 17:47:58 +0100

    update apagartabelas sql e teste postman txt

--- Base_de_Dados/API/apagar tabelas.sql
+++ Base_de_Dados/API/apagar tabelas.sql
@@ -16,3 +16,22 @@ drop table if exists public."Lista_Favoritos";
 drop table if exists public."Pessoas";
 drop table if exists public."Tipo_Alertas";
 drop table if exists public."Reports";
+
+--selects
+select * from public."Pessoas";
+select * from public."Outros_Utils";
+select * from public."Utils_Instituicaos";
+select * from public."Reports";
+select * from public."Tabela_LikesDislikes"
+--delete from public."Tabela_LikesDislikes"
+--update public."Pessoas" set "Email" ='martinhoantigo' where "Email" ='martinho';
+--delete from public."Pessoas" where "IDPessoa" >= 38;
+--delete from public."Outros_Utils" where "PessoaIDPessoa" >= 38;
+--update public."Utils_Instituicaos" set "Verificado" ='true' where "ID_Util" ='13';
+
+select * from "Report_Outdoor_Outros_Utils";
+select * from "Report_Outdoor_Util_Instituicaos";
+select "Data" from "Report_Outdoor_Outros_Utils" inner join "Reports"
+on "Reports"."ID_Report" ="Report_Outdoor_Outros_Utils"."ReportIDReport";
+select "Data" from "Report_Outdoor_Util_Instituicaos" inner join "Reports"
+on "Reports"."ID_Report" = "Report_Outdoor_Util_Instituicaos"."ReportIDReport";
