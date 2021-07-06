c909800e2b0f9352a4cbf592b4c6cfbd8ca23a1f
DiogoTojal7 <estgv17172@alunos.estgv.ipv.pt>
2021-07-06 17:01:21 +0100

    Inicio utilizadores_inst

---
+++ Base_de_Dados/API/src/routes/utilizadores_instituicao.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+//importar os controllers
+const utilizadoresController = require('../controllers/utilizadores_instituicao');
+const middleware = require('../middleware');
+
+router.get('/listutilizadores', utilizadoresController.)
