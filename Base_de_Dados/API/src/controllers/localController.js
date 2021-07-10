38e3ba78b2530f7a372eb2d1f440ef9e6a8ccf9a
marto <martinho.tavares@live.com.pt>
2021-07-10 20:46:06 +0100

    rota com percentagens de reports de densidade alte

--- Base_de_Dados/API/src/controllers/localController.js
+++ Base_de_Dados/API/src/controllers/localController.js
@@ -1,10 +1,15 @@
 //todo: fazer listagem de locais
 //todo: depois ir ao mobile, apanhar esses locais e tentar fazer reports
 var sequelize = require('../model/database');
+const {Op} = require('sequelize')
+const Local = require('../model/Local');
 const controllers = {}
 const locais = require('../model/Local');
 const Local_Indoor = require('../model/Local_Indoor');
 const localindoor = require('../model/Local_Indoor')
+const Report = require('../model/Reports/Report')
+const Report_Outdoor_Outros_Util = require('../model/Reports/Report_Outdoor_Outros_Util')
+const Report_Outdoor_Util_Instituicao = require('../model/Reports/Report_Outdoor_Util_Instituicao')
 controllers.listarLocais = async(req,res)=>{
     var statuscode = 200;
     var errMessage="";
@@ -95,6 +100,99 @@ controllers.getListaLocaisIndoor = async(req,res)=>{
         res.status(500).send({desc:"erro a pesquisar",err:e.original})
     }
     res.send({LocaisIndoor:listaLocaisIndoor})
+}
+
+/**
+ * 
+ * vai devolver todos os locais e a sua percentagem relativamente ao numero de reports de grande densidade 
+ */
+controllers.getPercentagemDeReportsDeCadaLocal = async(req,res)=>{//post
+    const {tempo, tipoTempo,niveldensidade} = req.body //tipotempo = hh - horas, mm - minutos, dd - dias
+    let dataAgr = new Date()
+    switch(tipoTempo){
+        case 'hh':{
+            dataAgr.setHours(dataAgr.getHours() - tempo)
+            break;
+        }
+        case 'mm':{
+            dataAgr.setMinutes(dataAgr.getMinutes() - tempo)
+            break;
+        }
+        case 'dd':{
+            dataAgr.setDate(dataAgr.getDate() - tempo)
+            break;
+        }
+        default:{
+            res.status(500).send({desc:"Tipo Invalido",err:"TipoInvalido"})
+            return;
+        }
+    }
+    if(typeof(niveldensidade).toString() !== 'number'){
+        res.status(500).send({desc:'Numero de densidade nao e um numero'})
+        return;
+    }else if(niveldensidade > 3 || niveldensidade < 1){
+        res.status(500).send({desc:'Numero de densidade invalido'})
+        return;
+    }
+    dataAgr = dataAgr.toISOString()
+    var listalocaisEReports = new Array()
+    var nReportsTotal = 0
+    try {
+        var listaDeLocais = await Local.findAll()
+        for(let locale of listaDeLocais){//obter lista de reports, contar numero de reports, guardar num json, meter na lista
+            let reportoutros=await Report_Outdoor_Outros_Util.findAll({
+                include:{
+                    model:Report,
+                    where:{
+                        Nivel_Densidade:{
+                            [Op.gt]:niveldensidade
+                        },Data:{
+                            [Op.gte]:dataAgr
+                        }
+                    }
+                },
+                where:{
+                    LocalIDLocal:locale.dataValues.ID_Local
+                }
+            })
+            let reportutils=await Report_Outdoor_Util_Instituicao.findAll({
+                include:{
+                    model:Report,
+                    where:{
+                        Nivel_Densidade:{
+                            [Op.gt]:niveldensidade
+                        },Data:{
+                            [Op.gte]:dataAgr
+                        }
+                    }
+                },
+                where:{
+                    LocalIDLocal:locale.dataValues.ID_Local
+                }
+            })
+            nReportsTotal = nReportsTotal +reportutils.length + reportoutros.length
+            
+            let localjson = {
+                NomeLocal:locale.dataValues.Nome, IDLocal:locale.dataValues.ID_Local,
+                NumeroReports:(reportutils.length + reportoutros.length)
+            }
+            console.log(localjson)
+            listalocaisEReports.push(localjson)
+        }
+    } catch (e) {
+        console.log(e)
+        res.status(500).send({desc:'Erro a selecionar',err:e.original})
+    }
+    var resposta = new Array();
+    for(let local of listalocaisEReports){
+        let percentagemlocal = (local.NumeroReports / nReportsTotal) *100
+        resposta.push({
+            IDLocal:local.IDLocal,
+            NomeLocal:local.NomeLocal,
+            PercentagemLocal:percentagemlocal
+        })
+    }
+    res.send({Resultado:resposta})
 }
 
 module.exports = controllers;
--- Base_de_Dados/API/src/routes/localsRoute.js
+++ Base_de_Dados/API/src/routes/localsRoute.js
@@ -8,6 +8,6 @@ router.post('/criar_novo_local_indoor',localController.criarLocalIndoor)
 router.get('/apagar_local_indoor/:idLocal/:idLocalIndoor',localController.apagarLocalIndoor)
 router.post('/criar_novo_local',localController.criarLocal)
 router.get('/get_lista_locais_indoor_local/:idLocal',localController.getListaLocaisIndoor)
-
+router.post('/percentagem_reports_locais',localController.getPercentagemDeReportsDeCadaLocal)
 
 module.exports = router;
