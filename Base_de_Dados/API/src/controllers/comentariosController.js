fafdb6d9fd1b0565f0f4d91e84c2552e2227397a
marto <martinho.tavares@live.com.pt>
2021-07-12 17:11:51 +0100

    rotas comentarios, criar apagar update get

--- Base_de_Dados/API/src/controllers/comentariosController.js
+++ Base_de_Dados/API/src/controllers/comentariosController.js
@@ -1,6 +1,7 @@
 var sequelize = require('../model/database');
 const controllers = {}
-const Comentario = require('../model/Comentarios')
+const Comentario = require('../model/Comentarios');
+const e = require('express');
 
 controllers.criarComentario = async (req,res) => { //post
     const { Descricao,Classificacao,IDLocal,IDPessoa}= req.body
@@ -8,13 +9,19 @@ controllers.criarComentario = async (req,res) => { //post
     let dataAgr = new Date()
     dataAgr = dataAgr.toISOString()
     try{
-        var Comentarios = await Comentario.create({
-            Descricao: Descricao,
-            Classificacao:Classificacao,
-            Data: dataAgr,
-            LocalIDLocal: IDLocal,
-            PessoaIDPessoa:IDPessoa
+        var jaFezComent = await Comentario.count({
+            where:{
+                LocalIDLocal:IDLocal, PessoaIDPessoa:IDPessoa
+            }
         })
+        if(jaFezComent === 0)
+            var Comentarios = await Comentario.create({
+                Descricao: Descricao,
+                Classificacao:Classificacao,
+                Data: dataAgr,
+                LocalIDLocal: IDLocal,
+                PessoaIDPessoa:IDPessoa
+            })
     }catch(e){
         console.log(e);
         statusCode = 500;
@@ -23,7 +30,79 @@ controllers.criarComentario = async (req,res) => { //post
 
     if(statusCode === 500)
         res.status(statusCode).send({status:statusCode, desc:"Erro a criar"})
-    else res.status(statusCode).send({status:statusCode,Comentario: Comentarios })
+    else if(jaFezComent !== 0){
+        res.status(500).send({desc:'Esta pessoa ja deu a sua opiniao neste local'})
+    } 
+    else{res.status(statusCode).send({status:statusCode,Comentario: Comentarios })}
 } 
+
+controllers.getComentario= async(req,res)=>{ //post
+    const {IDLocal,IDPessoa}= req.params
+    try{
+        var comentario = await Comentario.findOne({
+            where:{
+                LocalIDLocal:IDLocal, PessoaIDPessoa:IDPessoa
+            }
+        })
+    }catch(e){
+        console.log(e)
+        res.status(500).send({desc:'Erro a selecionar', err:e.original})
+    }
+    if(comentario !== null)
+        res.send({Comentario:comentario})
+    else res.status(500).send({desc:'Esta pessoa nao deu opiniao sobre este local'})
+}
+
+controllers.updateComentario = async(req,res)=>{ //post
+    const { Descricao,Classificacao,IDLocal,IDPessoa, Data}= req.body
+    let dataAgr = new Date()
+    dataAgr = dataAgr.toISOString()
+    try {
+        var jaFezComent = await Comentario.count({
+            where:{
+                LocalIDLocal:IDLocal, PessoaIDPessoa:IDPessoa
+            }
+        })
+        if(jaFezComent !==0)
+        {
+            var comentario = await Comentario.findOne({
+                where:{
+                    Data:Data,
+                    LocalIDLocal:IDLocal,
+                    PessoaIDPessoa:IDPessoa
+                }
+            })
+            comentario.Descricao = Descricao
+            comentario.Classificacao = Classificacao
+            comentario.Data = dataAgr
+            await comentario.save()
+        }
+    } catch (e) {
+        console.log(e)
+        res.send({desc:"Erro a fazer update",err:e.original})
+    }
+    if(jaFezComent === 0)
+        res.status(500).send({desc:'Esta pessoa nao deu opiniao sobre este local'})
+    else res.send({Comentario:comentario})
+}
+
+controllers.apagarComentario = async(req,res)=>{
+    const {IDLocal,IDPessoa}= req.params
+    try{
+        var comentario = await Comentario.findOne({
+            where:{
+                LocalIDLocal:IDLocal, PessoaIDPessoa:IDPessoa
+            }
+        })
+        if(comentario !== null)
+        await comentario.destroy()
+    }catch(e){
+        console.log(e)
+        res.status(500).send({desc:'Erro a apagar', err:e.original})
+    }
+    if(comentario !== null)
+        res.send({desc:'Apagado com sucesso'})
+    else res.status(500).send({desc:'Esta pessoa nao deu opiniao sobre este local'})
+}
 
 module.exports = controllers;
--- Base_de_Dados/API/src/routes/comentariosRoute.js
+++ Base_de_Dados/API/src/routes/comentariosRoute.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const comentariosController = require('../controllers/comentariosController')
 
 router.post('/novo_comentario',comentariosController.criarComentario);
-
+router.post('/update_comentario',comentariosController.updateComentario);
+router.get('/get_comentario/:IDLocal/:IDPessoa',comentariosController.getComentario)
+router.delete('/delete_comentario/:IDLocal/:IDPessoa',comentariosController.apagarComentario)
 module.exports=router
