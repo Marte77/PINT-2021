840fd31d4cebf6c54f609d202b9106af13452c11
2170344 <2170344@users.noreply.github.com>
2021-07-15 18:19:45 +0100

    Conclusao opinoes
    Conclusao opinoes

--- Base_de_Dados/API/src/routes/comentariosRoute.js
+++ Base_de_Dados/API/src/routes/comentariosRoute.js
@@ -6,5 +6,6 @@ router.post('/novo_comentario',comentariosController.criarComentario);
 router.post('/update_comentario',comentariosController.updateComentario);
 router.get('/get_comentario/:IDLocal/:IDPessoa',comentariosController.getComentario)
 router.delete('/delete_comentario/:IDLocal/:IDPessoa',comentariosController.apagarComentario)
+router.post('/deletecomentario/:ID',comentariosController.delete_Comentario)
 router.get('/opinioes/:idInstituicao', comentariosController.get_comentarios);
 module.exports=router
